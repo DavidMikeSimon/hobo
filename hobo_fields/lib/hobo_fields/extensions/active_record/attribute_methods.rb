8e67ce92e07c899126bbbcc91cdc3685401c9861
David Simon <david.mike.simon@gmail.com>
2016-04-12 17:21:16 -0400

    Reducing overhead of read_attribute in hobo_fields

--- hobo_fields/lib/hobo_fields/extensions/active_record/attribute_methods.rb
+++ hobo_fields/lib/hobo_fields/extensions/active_record/attribute_methods.rb
@@ -1,17 +1,15 @@
 ActiveRecord::Base.class_eval do
   def _read_attribute_with_hobo(attr_name)
-    name = attr_name.to_s
-    if self.class.can_wrap_with_hobo_type?(name)
-      attr_name = attr_name.to_sym
-      val = _read_attribute_without_hobo(name)
-      wrapper_type = self.class.attr_type(attr_name)
+    if self.class.can_wrap_with_hobo_type?(attr_name)
+      val = _read_attribute_without_hobo(attr_name)
+      wrapper_type = self.class.attr_type(attr_name.to_sym)
       if HoboFields.can_wrap?(wrapper_type, val)
         wrapper_type.new(val)
       else
         val
       end
     else
-      _read_attribute_without_hobo(name)
+      _read_attribute_without_hobo(attr_name)
     end
   end
   alias_method_chain :_read_attribute, :hobo
@@ -23,13 +21,14 @@ ActiveRecord::Base.class_eval do
       if @can_wrap_cache.include?(attr_name)
         @can_wrap_cache[attr_name]
       else
-        @can_wrap_cache[attr_name] = \
+        @can_wrap_cache[attr_name.to_s] = \
           if connected?
             type_wrapper = try.attr_type(attr_name)
             type_wrapper.is_a?(Class) && type_wrapper.not_in?(HoboFields::PLAIN_TYPES.values)
           else
             false
           end
+        @can_wrap_cache[attr_name.to_sym] = @can_wrap_cache[attr_name.to_s]
       end
     end
 
